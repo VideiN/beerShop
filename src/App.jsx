f2e01f1cafc2bc1c6c89297cd4734f144e9a9cc7
임채완 <niediv@icloud.com>
2025-03-25 10:27:10 +0900

    libraries install and Routes, Components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {Routes, Route, Link, useNavigate} from 'react-router-dom';
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
 function App() {
 
